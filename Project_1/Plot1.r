e4d708fbdd75b24d028c3b27d754fb4bab31180a
chacheluca <javiersanchez84@ias.csic.es>
2015-04-12 20:07:28 +0200

    Update Plot1.r

--- Project_1/Plot1.r
+++ Project_1/Plot1.r
@@ -12,12 +12,12 @@ data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
 
 # Creating Plot
-png("plot2.png", width=480, height=480)
+png("plot1.png", width=480, height=480)
 
 plot(data$DateTime, data$Global_active_power, type= "1", lwd=1,  ylab="Global Active Power (kilowatts)", xlab="")
 
 #Saving to file
 
-dev.copy(png, file="plot2.png", height=480, width=480)
+dev.copy(png, file="plot1.png", height=480, width=480)
 
 dev.off()
